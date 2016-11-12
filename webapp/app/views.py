4a162979d6feebdf491fc61eb567f56890dda97b
Pradeet Swamy <pradeet1995@gmail.com>
2016-11-13 03:00:09 +0530

    Able to place a pointer at desired location. TODO: integrate image parser to get title of marker.

--- webapp/app/views.py
+++ webapp/app/views.py
@@ -5,6 +5,33 @@ from app import app
 from flask_googlemaps import GoogleMaps
 from flask_googlemaps import Map, icons
 
+markers = [
+    {
+        'icon': '//maps.google.com/mapfiles/ms/icons/green-dot.png',
+        'lat': 23.195746,
+        'lng': 72.632920,
+        'infobox': "Hello I am <b style='color:green;'>GREEN</b>!"
+    },
+    {
+        'icon': '//maps.google.com/mapfiles/ms/icons/blue-dot.png',
+        'lat': 23.186004,
+        'lng': 72.631472,
+        'infobox': "Hello I am <b style='color:blue;'>BLUE</b>!"
+    },
+    {
+        'icon': icons.dots.yellow,
+        'title': 'Click Here',
+        'lat': 23.183353,
+        'lng': 72.629173,
+        'infobox': (
+            "Hello I am <b style='color:#ffcc00;'>YELLOW</b>!"
+            "<h2>It is HTML title</h2>"
+            "<img src='//placehold.it/50'>"
+            "<br>Images allowed!"
+        )
+    }
+]
+
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
 
@@ -17,14 +44,9 @@ def clearUploadFolder():
         else:
             shutil.rmtree(file_object_path)
 
-@app.route('/r')
-def r():
-    print(request)
-
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 def index():
-    markers = []
     latitude = 23.1890566
     longitude = 72.6220352
 
@@ -32,13 +54,26 @@ def index():
         latitude = float(request.form['inputlat'])
         longitude = float(request.form['inputlong'])
         num_of_images = int(request.form['num-of-images'])
-        files = request.files['image-file']
+        files = []
+        for i in range(num_of_images):
+            files.append(request.files['image-file' + str(i)])
 
-        if files and allowed_file(files.filename):
-            filename = secure_filename(files.filename)
-            files.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        clearUploadFolder()
 
-        return redirect(url_for('index'))
+        for i, _file in enumerate(files):
+            if _file and allowed_file(_file.filename):
+                filename = secure_filename(_file.filename)
+                _file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                imageFormat = _file.filename.rsplit('.', 1)[1]
+                os.rename(os.path.join(app.config['UPLOAD_FOLDER'], filename), os.path.join(app.config['UPLOAD_FOLDER'], 'Image' + str(i) + '.' + imageFormat))
+
+        newMarker = {
+            'icon': icons.dots.yellow,
+            'title': 'Title goes here',
+            'lat': latitude,
+            'lng': longitude,
+        }
+        markers.append(newMarker)
 
     mymap = Map(
         identifier="mymap",
@@ -53,32 +88,7 @@ def index():
         ),
         lat=latitude,
         lng=longitude,
-        markers=[
-            {
-                'icon': '//maps.google.com/mapfiles/ms/icons/green-dot.png',
-                'lat': 23.195746,
-                'lng': 72.632920,
-                'infobox': "Hello I am <b style='color:green;'>GREEN</b>!"
-            },
-            {
-                'icon': '//maps.google.com/mapfiles/ms/icons/blue-dot.png',
-                'lat': 23.186004,
-                'lng': 72.631472,
-                'infobox': "Hello I am <b style='color:blue;'>BLUE</b>!"
-            },
-            {
-                'icon': icons.dots.yellow,
-                'title': 'Click Here',
-                'lat': 23.183353,
-                'lng': 72.629173,
-                'infobox': (
-                    "Hello I am <b style='color:#ffcc00;'>YELLOW</b>!"
-                    "<h2>It is HTML title</h2>"
-                    "<img src='//placehold.it/50'>"
-                    "<br>Images allowed!"
-                )
-            }
-        ]
+        markers=markers
     )
     return render_template('index.html', mymap=mymap)
 
